bd25b07b6c50127a7ecd1821f01f50c02e77a75f
wanlan.yu <wanlan.yu@1081519838788313>
2020-05-06 21:45:57 +0800

    merge

---
+++ components/header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <div class="container">
+      my_blog
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+
+}
+</style>
